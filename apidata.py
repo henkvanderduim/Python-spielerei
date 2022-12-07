27e583163f0333e91b2a270df011201efeeecb03
Henk van der Duim <henk.vanderduim@gmail.com>
2022-12-07 13:29:10 +0100

    typefout

--- apidata.py
+++ apidata.py
@@ -3,7 +3,7 @@ import requests
 
 # URL to scrape
 url = "http://api.exchangeratesapi.io/v1/latest?base=EUR&access_key=API-KEY-HERE"
-reponse = requests.get(url)
+response = requests.get(url)
 
 df = pd.DataFrame(response.json())
 df.to_csv("file_name.csv")
